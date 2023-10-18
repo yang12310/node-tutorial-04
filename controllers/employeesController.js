4bfbf836d20c76c7ebdae9ffc215f88c71e7f79a
yang12310 <yang12310@naver.com>
2023-10-18 20:19:30 +0900

    node-tutorial-04_2

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -1,27 +1,56 @@
-const data = {};
-data.employees = require("../model/employees.json");
+const data = {
+  employees: require('../model/employees.json'),
+  setEmployees: function (data) { this.employees = data }
+  }
 
 const getAllEmployees = (req, res) => {
   res.json(data.employees);
 };
+
 const createNewEmployee = (req, res) => {
-  res.json({
-    "firstname": req.body.firstname,
-    "lastname": req.body.lastname,
-  });
-};
-const updateEmployee = (req, res) => {
-  res.json({
-    "firstname": req.body.firstname,
-    "lastname": req.body.lastname,
-  });
-};
-const deleteEmployee = (req, res) => {
-  res.json({ id: req.body.id });
-};
-const getEmployee = (req, res) => {
-  res.json({ id: req.params.id });
-};
+  const newEmployee = {
+  id: data.employees?.length ? data.employees[data.employees.length - 1].id + 1 : 1,
+  firstname: req.body.firstname,
+  lastname: req.body.lastname
+  }
+  if (!newEmployee.firstname || !newEmployee.lastname) {
+    //둘중에 하나라도 undefind값이 나온다면
+  return res.status(400).json({ 'message': 'First and last names are required.' });
+  }
+  data.setEmployees([...data.employees, newEmployee]);
+    res.status(201).json(data.employees);
+  }
+
+  const updateEmployee = (req, res) => {
+  const employee = data.employees.find(emp => emp.id === parseInt(req.body.id));
+  if (!employee) {
+  return res.status(400).json({ "message": `Employee ID ${req.body.id} not found` });
+  }
+  if (req.body.firstname) employee.firstname = req.body.firstname;
+  if (req.body.lastname) employee.lastname = req.body.lastname;
+  const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id));
+  const unsortedArray = [...filteredArray, employee];
+  data.setEmployees(unsortedArray.sort((a, b) => a.id > b.id ? 1 : a.id < b.id ? -1 : 0));
+  res.json(data.employees);
+  }
+  const deleteEmployee = (req, res) => {
+  const employee = data.employees.find(emp => emp.id === parseInt(req.body.id));
+  if (!employee) {
+    return res.status(400).json({ "message": `Employee ID ${req.body.id} not found` });
+  }
+  const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id));
+  data.setEmployees([...filteredArray]);
+  res.json(data.employees);
+  }
+
+  
+  const getEmployee = (req, res) => {
+  const employee = data.employees.find(emp => emp.id === parseInt(req.params.id));
+  if (!employee) {
+  return res.status(400).json({ "message": `Employee ID ${req.params.id} not found` });
+  }
+  res.json(employee);
+  }
 
 module.exports = {
   getAllEmployees,
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 3500;
 app.use(logger);
 
 //Cross Origin Resource Sharing
+//url이 없으면 항상 작동 
 app.use(cors(corsOptions));
 
 
@@ -22,9 +23,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // server static file
+//static folder를 지정하는게 중요 
+//url이 있으면 거기서 끝나서 밑으로 가지않음. 즉 그 밑은 다 무시
 app.use('/', express.static(path.join(__dirname, '/public')));
 
 //routes
+//router를 선언하는 것이 중요
 app.use('/', require('./routes/root'));
 app.use('/employees', require('./routes/api/employees'));
 
@@ -43,4 +47,4 @@ app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
-});
+}); //반드시 필요한 것 무한루프를 돌기위함
